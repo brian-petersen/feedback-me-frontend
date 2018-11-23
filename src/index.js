3d41dbf31e537d88d5ab57d1363c15902c1ef88e
Brian Petersen <skiwalk3r@gmail.com>
2018-11-23 09:53:31 -0700

    Starts home page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import App from './App';
 
 import './assets/bootstrap.scss';
+
+library.add(fas);
 
 ReactDOM.render(<App />, document.getElementById('root'));
